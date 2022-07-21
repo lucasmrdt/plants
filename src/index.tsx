a3946086661ad0333543d506ec52d78d8756cf21
Lucas Marandat <lucas.mrdt@gmail.com>
2022-07-21 19:53:34 +0200

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 import "c3/c3.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -16,6 +17,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
